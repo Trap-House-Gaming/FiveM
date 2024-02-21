f89c29cc8d1c38994d12c308c06447c6876df8b5
KIVG.LUIS <KIVG.LUIS.OFFICIAL@GMAIL.COM>
2024-02-21 16:05:59 -0600

    Removed

--- txData/QBCoreFramework_B06BFC.base/resources/[zr]/zr-locations/zr-config/zr-build-c.lua
+++
@@ -1,9 +0,0 @@
-function zr_locations_show_public()
-	-- Trigger Custom Functions/Events when display menu.
-	DisplayRadar(false)
-end
-
-function zr_locations_hide_public()
-	-- Trigger Custom Functions/Events when hide menu.
-	DisplayRadar(true)
-end
