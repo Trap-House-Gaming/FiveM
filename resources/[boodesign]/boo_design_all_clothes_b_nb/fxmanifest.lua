2b23780775c979994e2019cc7569be570aca655d
KIVG.LUIS <thgdayzserver@gmail.com>
2024-02-26 19:04:51 -0600

    [boodesign] 3/3

---
+++ resources/[boodesign]/boo_design_all_clothes_b_nb/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Boodesign discord|https://discord.gg/XDbhUnWvTt
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_boodesignstudio2.meta',
+  'mp_f_freemode_01_boodesignstudio2.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_boodesignstudio2.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_boodesignstudio2.meta'
+dependency '/assetpacks'
+dependency '/assetpacks'
+dependency '/assetpacks'
