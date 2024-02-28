4f919f252eac01fbfeaa2a1d07c24f45ec2c6633
KIVG.LUIS <thgdayzserver@gmail.com>
2024-02-28 10:19:46 -0600

    Added police EUP

---
+++ resources/[standalone]/LSPD EUP/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Boodesign discord|https://discord.gg/XDbhUnWvTt
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 
+data_file 'SHOP_PED_APPAREL_META_FILE' 
+dependency '/assetpacks'
+dependency '/assetpacks'
+dependency '/assetpacks'
