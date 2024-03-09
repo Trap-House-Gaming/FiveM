6487b5b11c7cc3f55f99da5414e3619b54faa8d8
SERIOUS <cobeyking@live.com>
2024-03-09 13:43:16 -0600

    added some framework for a few businesses

--- resources/[qb]/qb-management/client/cl_config.lua
+++ resources/[qb]/qb-management/client/cl_config.lua
@@ -42,6 +42,42 @@ Config.BossMenuZones = {
     ['mechanic'] = {
         { coords = vector3(-339.53, -156.44, 44.59), length = 1.15, width = 2.6, heading = 353.0, minZ = 43.59, maxZ = 44.99 },
     },
+	['chickfila'] = {
+    vector3(-1035.65, -1371.11, 5.52), --place this in qb-management/client/cl_config.lua
+	},
+	['dominos'] = {
+		vector3(541.36, 114.08, 96.57), --place this in qb-management/client/cl_config.lua
+	},
+	['dunkin'] = {
+		vector3(-584.77, -883.61, 25.95), --place this in qb-management/client/cl_config.lua
+	},
+	['icebox'] = {
+		vector3(-1258.61, -805.02, 17.09), --place this in qb-management/client/cl_config.lua
+	},
+	['mcdonalds'] = {
+		vector3(84.36, 293.48, 110.21), --place this in qb-management/client/cl_config.lua
+	},
+	['pearls'] = {
+		vector3(-1837.34, -1178.89, 19.22), --place this in qb-management/client/cl_config.lua
+	},
+	['popeyes'] = {
+		vector3(179.1, -1456.12, 29.13), --place this in qb-management/client/cl_config.lua
+	},
+	['taco'] = {
+		vector3(20.22, -1602.11, 29.38), --place this in qb-management/client/cl_config.lua
+	},
+	['wingstop'] = {
+		vector3(1244.08, -352.53, 69.18), --place this in qb-management/client/cl_config.lua
+	},
+	['aldentes'] = {
+		vector3(-586.05, -607.51, 34.68), --place this in qb-management/client/cl_config.lua
+	},
+	['cluckinbell'] = {
+		vector3(-586.04, -597.81, 34.68), --place this in qb-management/client/cl_config.lua
+	},
+	['craftbar'] = {
+		vector3(119.04, -1045.56, 29.31), --place this in qb-management/client/cl_config.lua
+	},
 }
 
 Config.GangMenus = {
