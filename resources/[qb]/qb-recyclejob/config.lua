30baab37f7ae7e5d40e8419cba2466296d8cbbd3
KIVG.LUIS <thgdayzserver@gmail.com>
2024-03-15 14:06:15 -0500

    Fixes

--- resources/[qb]/qb-recyclejob/config.lua
+++ resources/[qb]/qb-recyclejob/config.lua
@@ -2,7 +2,7 @@ Config = {
 	-- **** IMPORTANT ****
 	-- UseTarget should only be set to true when using qb-target
 	Config.Usetarget = false
-	UseTarget = GetConvar('UseTarget', 'false') == 'false',
+	--UseTarget = GetConvar('UseTarget', 'false') == 'false',
 
 	OutsideLocation 	= vector4(55.55, 6472.18, 31.43, 44.0),
 	InsideLocation 		= vector4(1073.0, -3102.49, -39.0, 266.61),
