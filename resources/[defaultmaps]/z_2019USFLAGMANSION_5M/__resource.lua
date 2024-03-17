3edfad3ccbd34a508c82ed812524c0a2d3023640
SERIOUS <cobeyking@live.com>
2024-03-17 15:25:39 -0500

    added two mlos

---
+++ resources/[defaultmaps]/z_2019USFLAGMANSION_5M/__resource.lua
@@ -0,0 +1,15 @@
+this_is_a_map 'yes'
+
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+data_file 'DLC_ITYP_REQUEST' 'stream/increasedprops_interiors.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/increasedprops_terrain.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/v_minigame.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/v_industrial_2.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/increasedprops_props.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/builderdefaddons.ytyp'
