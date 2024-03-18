0720e7e3fdc43a9741778d41df9174fd393b94d0
KIVG.LUIS <thgdayzserver@gmail.com>
2024-03-18 16:21:44 -0500

    Pengu Test Fix

--- resources/[standalone]/pengu_gruppe6delivery/config.lua
+++ resources/[standalone]/pengu_gruppe6delivery/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Notify = 'ox' -- 'qb' or 'ox' or 'none'
-Config.Target = 'ox' -- 'qb' or 'ox' or 'interact' [https://github.com/darktrovx/interact]
+Config.Target = 'qb' -- 'qb' or 'ox' or 'interact' [https://github.com/darktrovx/interact]
 Config.InterctView = 7.5 -- Only used if Config.Target == 'interact' | Does not affect 'qb' or 'ox'
 Config.InteractDist = 2.5 -- Only used if Config.Target == 'interact' | Does not affect 'qb' or 'ox'
 Config.Menu = 'qb' -- 'qb' or 'ox'
