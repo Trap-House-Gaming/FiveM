d6688c51907f9e67aa4f61e4cde00a525346b9a8
SERIOUS <cobeyking@live.com>
2024-03-19 01:24:23 -0500

    tunes

---
+++ resources/[standalone]/[CarSounds]/tjz1eng/__resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    'audioconfig/tjz1eng_amp.dat10.rel',
+	'audioconfig/tjz1eng_game.dat151.rel',
+	'audioconfig/tjz1eng_sounds.dat54.rel',
+	'sfx/dlc_tjz1eng/tjz1eng.awc',
+	'sfx/dlc_tjz1eng/tjz1eng_npc.awc',
+}
+
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/tjz1eng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tjz1eng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tjz1eng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tjz1eng'
+
+
