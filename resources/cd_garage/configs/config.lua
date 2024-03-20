948add702b9a5eef54ee7532e457e8941f1d036c
SERIOUS <cobeyking@live.com>
2024-03-20 00:45:38 -0500

    fix

--- resources/cd_garage/configs/config.lua
+++ resources/cd_garage/configs/config.lua
@@ -635,7 +635,7 @@ end
 
 Config.Locations = {
     {
-        Garage_ID = 'pillbox', --PINK MOTEL
+        Garage_ID = 'Alta Garage', --PINK MOTEL
         Type = 'car',
         Dist = 10,
         x_1 = 215.09, y_1 = -805.17, z_1 = 30.81,
@@ -648,7 +648,7 @@ Config.Locations = {
         ShellType = '10cargarage_shell',
     },
     {
-        Garage_ID = 'Alta Garage', --The very first car garage's `garage_id` must be the same as the default value of the `garage_id` in the database as when a vehicle is purchased it gets sent to this garage. You can change the garage id's to what ever you like but make sure to also change the default garage_id in the database.
+        Garage_ID = 'pillbox', --The very first car garage's `garage_id` must be the same as the default value of the `garage_id` in the database as when a vehicle is purchased it gets sent to this garage. You can change the garage id's to what ever you like but make sure to also change the default garage_id in the database.
         Type = 'car', --The type of vehicles which use this garage. ('car'/'boat'/'air').
         Dist = 10, --The distance that you can use this garage.
         x_1 = -304.97, y_1 = -985.45, z_1 = 31.08, --This is the location of the garage, where you press e to open for example.
