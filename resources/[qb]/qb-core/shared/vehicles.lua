bb5d52665713cd40dc06c365a494a47181153fe7
SERIOUS <cobeyking@live.com>
2024-03-21 17:52:33 -0500

    car

--- resources/[qb]/qb-core/shared/vehicles.lua
+++ resources/[qb]/qb-core/shared/vehicles.lua
@@ -769,7 +769,10 @@ local Vehicles = {
     { model = 'oycfury',        name = '720s',                         brand = 'McLaren',        price = 500000,  category = 'Import',      type = 'automobile', shop = 'none' },
     { model = 'GODzR35BOSS',    name = 'R35',                          brand = 'Nissan',        price = 500000,  category = 'Import',      type = 'automobile', shop = 'none' },
     { model = 'rmodm3joker',    name = 'M3',                           brand = 'BMW',        price = 500000,  category = 'Import',      type = 'automobile', shop = 'none' },
-    { model = 'vc_pol765lt',    name = '765lt',                        brand = 'McLaren',        price = 500000,  category = 'Import',      type = 'automobile', shop = 'none' }
+    { model = 'vc_pol765lt',    name = '765lt',                        brand = 'McLaren',        price = 500000,  category = 'Import',      type = 'automobile', shop = 'none' },
+    { model = 'm135iwb',        name = 'M135 I',                       brand = 'BMW',        price = 500000,  category = 'Import',      type = 'automobile', shop = 'none' }
+
+
 }
 
 for i = 1, #Vehicles do
