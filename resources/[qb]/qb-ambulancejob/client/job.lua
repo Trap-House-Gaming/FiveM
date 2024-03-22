a06c3b1943784142212069d1d41c77df01b9e3e3
SERIOUS <cobeyking@live.com>
2024-03-21 22:10:16 -0500

    status check removal

--- resources/[qb]/qb-ambulancejob/client/job.lua
+++ resources/[qb]/qb-ambulancejob/client/job.lua
@@ -196,22 +196,7 @@ RegisterNetEvent('hospital:client:CheckStatus', function()
                             bone = Config.BoneIndexes[k],
                             label = v.label .. ' (' .. Config.WoundStates[v.severity] .. ')'
                         }
-                    elseif result['WEAPONWOUNDS'] then
-                        for _, v2 in pairs(result['WEAPONWOUNDS']) do
-                            TriggerEvent('chat:addMessage', {
-                                color = { 255, 0, 0 },
-                                multiline = false,
-                                args = { Lang:t('info.status'), QBCore.Shared.Weapons[v2].damagereason }
-                            })
-                        end
-                    elseif result['BLEED'] > 0 then
-                        TriggerEvent('chat:addMessage', {
-                            color = { 255, 0, 0 },
-                            multiline = false,
-                            args = { Lang:t('info.status'),
-                                Lang:t('info.is_status', { status = Config.BleedingStates[v].label }) }
-                        })
-                    else
+                    elseif
                         QBCore.Functions.Notify(Lang:t('success.healthy_player'), 'success')
                     end
                 end
