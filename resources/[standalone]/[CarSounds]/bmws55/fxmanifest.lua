c0af4587236a742bb975aba1c43b578d0cdd173c
SERIOUS <cobeyking@live.com>
2024-04-04 18:19:48 -0500

    updates and fixes

---
+++ resources/[standalone]/[CarSounds]/bmws55/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/bmws55_game.dat151.rel',
+  'audioconfig/bmws55_sounds.dat54.rel',
+  'sfx/dlc_bmws55/bmws55.awc',
+  'sfx/dlc_bmws55/bmws55_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/bmws55_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/bmws55_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bmws55'
