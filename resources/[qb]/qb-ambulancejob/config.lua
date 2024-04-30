227b291701896e561cd668824f6aaf7e68c0985a
SERIOUS <cobeyking@live.com>
2024-04-29 20:31:27 -0500

    fixes

--- resources/[qb]/qb-ambulancejob/config.lua
+++ resources/[qb]/qb-ambulancejob/config.lua
@@ -45,7 +45,7 @@ Config.Locations = {                -- Edit the various interaction points for p
         [2] = vector3(-254.88, 6324.5, 32.58),
     },
     ['vehicle'] = {
-        [1] = vector4(337.31, -579.91, 43.179, 28.8),
+        [1] = vector4(337.31, -579.91, 28.8, 180),
         [2] = vector4(-234.28, 6329.16, 32.15, 222.5),
     },
     ['helicopter'] = {
@@ -60,10 +60,10 @@ Config.Locations = {                -- Edit the various interaction points for p
         [1] = vector4(338.5, -583.85, 74.16, 245.5),
     },
     ['main'] = {
-        [1] = vector3(327.14, -603.78, 43.28),
+        [1] = vector3(327.14, -603.78, 43.29),
     },
     ['stash'] = {
-        [1] = vector3(306.82, -601.83, 43.28),
+        [1] = vector3(306.82, -601.83, 43.29),
     },
     ["beds"] = {
         [1] = {coords = vector4(324.26, -582.77, 44.2, 158.15), taken = false, model = 1631638868},
